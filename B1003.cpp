4ab6aeb12bc2e6a4dcee5650c04c7c583469c76c
toughpig <378981240@qq.com>
2018-09-10 19:09:13 +0800

    Update B1003.cpp

--- B1003.cpp
+++ B1003.cpp
@@ -1,4 +1,29 @@
-//�ж� (A*n)+P+(A*k)+T+(A*(k*n))
+//更简洁的写法，参考柳神
+// #include <iostream>
+// #include <map>
+// using namespace std;
+// int main() {
+//     int n, p = 0, t = 0;
+//     string s;
+//     cin >> n;
+//     for(int i = 0; i < n; i++) {
+//         cin >> s;
+//         map<char, int> m;
+//         for(int j = 0; j < s.size(); j++) {
+//             m[s[j]]++;
+//             if (s[j] == 'P') p = j;
+//             if (s[j] == 'T') t = j;
+//         }
+//         if(m['P'] == 1 && m['A'] != 0 && m['T'] == 1 && m.size() == 3 && p * (t-p-1) == s.length()-t-1)
+//             printf("YES\n");
+//         else
+//             printf("NO\n");
+//     }
+//     return 0;
+// }
+
+
+//判断(A*n)+P+(A*k)+T+(A*(k*n))
 #include<cstdio>
 #include<string>
 #include<iostream>
