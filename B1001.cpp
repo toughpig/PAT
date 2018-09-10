67f16d749851d278d99fb7fdee35bebd7b3f0a68
toughpig <378981240@qq.com>
2018-09-10 18:55:01 +0800

    Add files via upload

---
+++ B1001.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+  int cnt=0;
+  int num;
+  scanf("%d",&num);
+  while(num!=1){
+    if(num%2){
+      num=(num*3+1)/2;
+      cnt++;
+    }
+    else{
+      num/=2;
+      cnt++;
+    }
+  }
+  printf("%d",cnt);
+  return 0;
+}
